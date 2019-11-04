8ea8e6a04d471c254e84bf1bded911695aaf4567
thevan96 <vothevan96@gmail.com>
2019-11-05 06:49:24 +0700

    Add symlink git

--- sync.sh
+++ sync.sh
@@ -5,4 +5,4 @@ ln -sf ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv
-
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
