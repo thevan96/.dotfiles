205071cde4b6f22900b902b8f985d38c330e76e6
thevan96 <vothevan96@gmail.com>
2019-11-05 08:05:02 +0700

    dotfiles -> .dotfiles

--- sync.sh
+++ sync.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -sf ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sf ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh/.zshenv ~/.zshenv
+ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
 
 # snippet
-ln -sf ~/dotfiles/UltiSnips/javascript.snippets ~/.config/nvim/UltiSnips/javascript.snippets
-ln -sf ~/dotfiles/UltiSnips/php.snippets ~/.config/nvim/UltiSnips/php.snippets
-ln -sf ~/dotfiles/UltiSnips/html.snippets ~/.config/nvim/UltiSnips/html.snippets
+ln -sf ~/.dotfiles/UltiSnips/javascript.snippets ~/.config/nvim/UltiSnips/javascript.snippets
+ln -sf ~/.dotfiles/UltiSnips/php.snippets ~/.config/nvim/UltiSnips/php.snippets
+ln -sf ~/.dotfiles/UltiSnips/html.snippets ~/.config/nvim/UltiSnips/html.snippets
 
 echo 'Sync done!'
