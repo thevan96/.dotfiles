60955f22699340225783d3136d0e66ada5d6a7dd
thevan96 <vothevan96@gmail.com>
2022-09-12 19:25:10 +0700

    fix(nvim-cmp): Limit max width completion menu nvim-cmp

--- dir-nvim/.config/nvim/lua/module_cmp.lua
+++ dir-nvim/.config/nvim/lua/module_cmp.lua
@@ -1,6 +1,23 @@
 local cmp = require('cmp')
 
+local ELLIPSIS_CHAR = '…'
+local MAX_LABEL_WIDTH = 25
+local MIN_LABEL_WIDTH = 25
+
 cmp.setup({
+  formatting = {
+    format = function(_, vim_item)
+      local label = vim_item.abbr
+      local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
+      if truncated_label ~= label then
+        vim_item.abbr = truncated_label .. ELLIPSIS_CHAR
+      elseif string.len(label) < MIN_LABEL_WIDTH then
+        local padding = string.rep(' ', MIN_LABEL_WIDTH - string.len(label))
+        vim_item.abbr = label .. padding
+      end
+      return vim_item
+    end,
+  },
   preselect = cmp.PreselectMode.None,
   completion = {
     autocomplete = false
